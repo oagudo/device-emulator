ab029c060a747d8d6bcad3ce8e939aec6982e069
Omar <omar.agudo@gmail.com>
2014-11-09 23:23:49 +0100

    Added TCP comm test

---
+++ test/src/Comms/TCP/TCPCommsTest.cpp
@@ -0,0 +1,43 @@
+#include <boost/test/auto_unit_test.hpp>
+
+#include <Comms/TCP/TCPClient.h>
+#include <Comms/TCP/TCPServer.h>
+
+using namespace device_emulator;
+
+BOOST_AUTO_TEST_SUITE( TCPCommsTests )
+
+struct Fixture {
+    IMessagePtr msg1;
+
+    TCPClientSetupPtr clientSetup;
+    TCPClientPtr client;
+
+    TCPServerSetupPtr serverSetup;
+    TCPServerPtr server;
+
+    Fixture() :
+        msg1(new Message(1, "msg1", "content1")),
+        clientSetup(new TCPClientSetup("localhost", "2222")),
+        client(new TCPClient(clientSetup)),
+        serverSetup(new TCPServerSetup("2222")),
+        server(new TCPServer(serverSetup)) { };
+};
+
+BOOST_AUTO_TEST_CASE( TCPCommsTests_ServerStartsListenningCorrectly ) {
+
+    Fixture f;
+    BOOST_CHECK (f.server->Start());
+    f.server->Close();
+}
+
+BOOST_AUTO_TEST_CASE( TCPCommsTests_ClientConnectsToServerCorrectly ) {
+
+    Fixture f;
+    f.server->Start();
+    BOOST_CHECK(f.client->Start());
+    f.server->Close();
+    f.client->Close();
+}
+
+BOOST_AUTO_TEST_SUITE_END()

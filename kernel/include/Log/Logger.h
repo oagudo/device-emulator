1e60be8a217fd740a8628371e1da07acc7cf0a85
oagudo <omar.agudo@gmail.com>
2014-11-14 00:00:52 +0100

    Logging init method added for setting the initial logging config

--- kernel/include/Log/Logger.h
+++ kernel/include/Log/Logger.h
@@ -1,9 +1,17 @@
 #ifndef _LOGGER_H
 #define _LOGGER_H
 
+#include <iostream>
+
 #ifdef WITH_LOG4CXX
 
-#include "log4cxx/logger.h"
+#include <log4cxx/xml/domconfigurator.h>
+#include <log4cxx/logger.h>
+#include <log4cxx/helpers/pool.h>
+#include <log4cxx/basicconfigurator.h>
+#include <log4cxx/fileappender.h>
+#include <log4cxx/simplelayout.h>
+
 #define DEFINE_LOGGER(VAR, NAME) static log4cxx::LoggerPtr VAR = log4cxx::Logger::getLogger(NAME);
 #define LOG_ERROR LOG4CXX_ERROR
 #define LOG_WARN LOG4CXX_WARN
@@ -11,12 +19,48 @@
 
 #else
 
-#include <iostream>
 #define DEFINE_LOGGER(VAR, NAME) static const char *VAR = NAME;
 #define LOG_ERROR(LOGGER, DATA) std::cerr << "E: <" << LOGGER << "> " << DATA << "\n";
 #define LOG_WARN(LOGGER, DATA) std::cout << "W: <" << LOGGER << "> " << DATA << "\n";
 #define LOG_INFO(LOGGER, DATA) std::cout << "I: <" << LOGGER << "> " << DATA << "\n";
 
 #endif // WITH_LOG4CXX
+
+namespace device_emulator {
+
+    /*!
+      \class Represents the logger facility of the library
+    */
+    class Logger {
+
+    public:
+
+        static void Init()
+        {
+#ifdef WITH_LOG4CXX
+            log4cxx::FileAppender * fileAppender = new
+                log4cxx::FileAppender(log4cxx::LayoutPtr(new log4cxx::SimpleLayout()),
+                                      "device_emulator.log", false);
+            log4cxx::helpers::Pool p;
+            fileAppender->activateOptions(p);
+            log4cxx::BasicConfigurator::configure(log4cxx::AppenderPtr(fileAppender));
+            log4cxx::Logger::getRootLogger()->setLevel(log4cxx::Level::getDebug());
+#endif
+        }
+
+        static void Configure(const std::string &fileName) {
+            try {
+#ifdef WITH_LOG4CXX
+                log4cxx::xml::DOMConfigurator::configure(fileName);
+#else
+                // Do nothing
+#endif
+            } catch (std::exception &e) {
+                std::cerr << "Error when configuring logging " << e.what() << std::endl;
+            }
+        }
+    };
+
+} // namespace
 
 #endif // LOGGER
--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,3 +1,18 @@
 #define BOOST_TEST_MODULE DeviceEmulator
 
 #include <boost/test/auto_unit_test.hpp>
+
+#include <Log/Logger.h>
+
+struct InitTests {
+    InitTests() {
+        device_emulator::Logger::Init();
+        if ( boost::filesystem::exists("log4cxx.xml") ) {
+            // Configures log4cxx
+            device_emulator::Logger::Configure("log4cxx.xml");
+        }
+    }
+    ~InitTests() { }
+};
+
+BOOST_GLOBAL_FIXTURE(InitTests)

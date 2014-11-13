e3d5236d27e1cdfd929add2088428e63af780bcc
oagudo <omar.agudo@gmail.com>
2014-11-13 21:39:24 +0100

    Added logging class

--- kernel/include/Log/Logger.h
+++ kernel/include/Log/Logger.h
@@ -4,18 +4,18 @@
 #ifdef WITH_LOG4CXX
 
 #include "log4cxx/logger.h"
-#define DEF_LOGGER(VAR, NAME) static log4cxx::LoggerPtr VAR = log4cxx::Logger::getLogger(NAME);
-#define L_ERROR LOG4CXX_ERROR
-#define L_WARN LOG4CXX_WARN
-#define L_INFO LOG4CXX_INFO
+#define DEFINE_LOGGER(VAR, NAME) static log4cxx::LoggerPtr VAR = log4cxx::Logger::getLogger(NAME);
+#define LOG_ERROR LOG4CXX_ERROR
+#define LOG_WARN LOG4CXX_WARN
+#define LOG_INFO LOG4CXX_INFO
 
 #else
 
 #include <iostream>
-#define DEF_LOGGER(VAR, NAME) static const char *VAR = NAME;
-#define L_ERROR(LOGGER, DATA) std::cerr << "E: <" << LOGGER << "> " << DATA << "\n";
-#define L_WARN(LOGGER, DATA) std::cout << "W: <" << LOGGER << "> " << DATA << "\n";
-#define L_INFO(LOGGER, DATA) std::cout << "I: <" << LOGGER << "> " << DATA << "\n";
+#define DEFINE_LOGGER(VAR, NAME) static const char *VAR = NAME;
+#define LOG_ERROR(LOGGER, DATA) std::cerr << "E: <" << LOGGER << "> " << DATA << "\n";
+#define LOG_WARN(LOGGER, DATA) std::cout << "W: <" << LOGGER << "> " << DATA << "\n";
+#define LOG_INFO(LOGGER, DATA) std::cout << "I: <" << LOGGER << "> " << DATA << "\n";
 
 #endif // WITH_LOG4CXX
 

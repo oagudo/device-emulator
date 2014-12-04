3e1712d875c68700de2ea1806957900faabcb5da
oagudo <omar.agudo@gmail.com>
2014-12-04 21:31:45 +0100

    Added more boost python bindings

--- kernel/src/Device/Orders/Order_ext.cpp
+++ kernel/src/Device/Orders/Order_ext.cpp
@@ -1,5 +1,9 @@
 #include "Device/Orders/IDeviceOrder.h"
 #include "Device/Orders/SendOrder.h"
+#include "Device/Orders/ReceiveOrder.h"
+#include "Device/Orders/WaitOrder.h"
+#include "Device/Orders/OrderList.h"
+#include "Device/Orders/InfiniteOrderList.h"
 #include <boost/python.hpp>
 
 using namespace boost::python;
@@ -13,12 +17,52 @@ struct IDeviceOrderWrap : IDeviceOrder, wrapper<IDeviceOrder>
     }
 };
 
+struct OrderListWrap : OrderList, wrapper<OrderList>
+{
+    IDeviceOrderPtr Next() {
+        if (override n = this->get_override("next"))
+			return n();
+		return OrderList::Next();
+    }
+
+    IDeviceOrderPtr default_Next() { return this->OrderList::Next(); }
+};
+
 void export_IDeviceOrders()
 {
+    class_<IDeviceOrderPtr>("IDeviceOrder")
+        ;
+
     class_<IDeviceOrderWrap, boost::noncopyable>("IDeviceOrder")
         .def("execute", pure_virtual(&IDeviceOrder::Execute))
         ;
 
-    class_<SendOrder, bases<IDeviceOrderWrap> >("SendOrder", init<const IMessagePtr&>())
+    class_<SendOrder, bases<IDeviceOrder> >("SendOrder", init<const IMessagePtr&>())
+        .def("execute", &SendOrder::Execute)
+        ;
+
+    class_<ReceiveOrder, bases<IDeviceOrder> >("ReceiveOrder", init<const unsigned int, const unsigned int>())
+        .def("execute", &ReceiveOrder::Execute)
+        ;
+
+    class_<WaitOrder, bases<IDeviceOrder> >("WaitOrder", init<const unsigned int>())
+        .def("execute", &WaitOrder::Execute)
+        ;
+
+    class_<OrderList, OrderListPtr>("OrderList")
+        ;
+
+    class_<InfiniteOrderList, bases<OrderList> >("InfiniteOrderList")
+        .def("next", &InfiniteOrderList::Next)
+        .def("add", &InfiniteOrderList::Add)
+        .def("empty", &InfiniteOrderList::Empty)
+        .def("count", &InfiniteOrderList::Count)
+        ;    
+
+    class_<OrderListWrap, boost::noncopyable>("OrderList")
+        .def("next", &OrderList::Next, &OrderListWrap::default_Next)
+        .def("add", &OrderList::Add)
+        .def("empty", &OrderList::Empty)
+        .def("count", &OrderList::Count)
         ;
 }

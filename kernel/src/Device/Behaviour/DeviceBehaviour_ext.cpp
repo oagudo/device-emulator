5e4277ec56e3cf6ce83233291f2d5991f1c1fac0
Omar <omar.agudo@gmail.com>
2014-12-06 18:26:30 +0100

    New exposed methods and properties

--- kernel/src/Device/Behaviour/DeviceBehaviour_ext.cpp
+++ kernel/src/Device/Behaviour/DeviceBehaviour_ext.cpp
@@ -1,4 +1,7 @@
 #include "Device/Behaviour/DeviceBehaviour.h"
+#include "Device/Behaviour/States/DeviceBehaviourState.h"
+#include "Device/Orders/OrderList.h"
+#include "Comms/CommunicationChannel.h"
 #include <boost/python.hpp>
 
 using namespace boost::python;
@@ -7,7 +10,13 @@ using namespace device_emulator;
 void export_DeviceBehaviour()
 {
     class_<DeviceBehaviour, DeviceBehaviourPtr, boost::noncopyable>("DeviceBehaviour", init<const std::string, const ComChannelPtr, const OrderListPtr>())
-        //        .def("to_string", pure_virtual(&IMessage::ToString))
+        .add_property("state", &DeviceBehaviour::GetState)
+        .add_property("name", &DeviceBehaviour::GetName)
+        .add_property("orders", &DeviceBehaviour::GetOrders)
+        .add_property("channel", &DeviceBehaviour::GetCommChannel)
+        .def("start", &DeviceBehaviour::Start)
+        .def("stop", &DeviceBehaviour::Stop)
+        .def("wait", &DeviceBehaviour::Wait)
         ;
 }
 

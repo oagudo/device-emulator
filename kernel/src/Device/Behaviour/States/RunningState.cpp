c78c084e547d92bb93dabf7f0be6ad30ca6bfa2d
Omar <omar.agudo@gmail.com>
2014-12-19 23:15:36 +0100

    Added missing files

---
+++ kernel/src/Device/Behaviour/States/RunningState.cpp
@@ -0,0 +1,39 @@
+#include "Device/Behaviour/States/RunningState.h"
+
+namespace device_emulator {
+
+RunningState::RunningState() {}
+
+DeviceBehaviourStatePtr RunningState::Instance() {
+    static DeviceBehaviourStatePtr instance(new RunningState());
+    return instance;
+}
+
+std::string RunningState::ToString() const {
+    return "Running";
+}
+
+void RunningState::Enter(const DeviceBehaviourPtr &context) const {
+    createExecutionThread(context);
+}
+
+void RunningState::ExecuteOrders(const DeviceBehaviourPtr &context) const {
+    while (!context->GetOrders()->Empty() &&
+           (context->GetState().get() == this)) {
+        context->GetOrders()->Next()->Execute(context);
+    }
+
+    if (context->GetState().get() == this) {
+        transitionTo(context, DeviceBehaviourStatePtr(new FinishedState()));
+    }
+}
+
+void RunningState::Stop(const DeviceBehaviourPtr &context) const {
+    transitionTo(context, StoppedState::Instance());
+}
+
+void RunningState::Wait(const DeviceBehaviourPtr &context) const {
+    waitExecutionThread(context);
+}
+
+} // namespace

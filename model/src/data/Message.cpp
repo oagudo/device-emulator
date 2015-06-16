cd7149ed01672f622decc75abaf906bd9dba43df
oagudo <omar.agudo@gmail.com>
2015-06-16 20:13:20 +0200

    Removed warnings

--- model/src/data/Message.cpp
+++ model/src/data/Message.cpp
@@ -3,11 +3,11 @@
 namespace device_emulator {
 
 Message::Message(const unsigned int msgID, const std::string &msgName, const std::string &msgContent) :
-                         _msgId(msgID), _msgName(msgName), _msgContent(msgContent) { };
+                         _msgId(msgID), _msgName(msgName), _msgContent(msgContent) { }
 
-Message::Message() : _msgId(0), _msgName(""), _msgContent("") { };
+Message::Message() : _msgId(0), _msgName(""), _msgContent("") { }
 
-Message::~Message() { };
+Message::~Message() { }
 
 std::string Message::ToString() const {
     return _msgContent;
